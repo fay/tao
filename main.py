9acd4c5b5bf757df1390139c1c05bb15d8853528
toozoofoo <toozoofoo@gmail.com>
2010-06-24 23:39:49 +0800

    support flask

---
+++ main.py
@@ -0,0 +1,38 @@
+# Copyright 2008 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Bootstrap for running a Django app under Google App Engine.
+
+The site-specific code is all in other files: settings.py, urls.py,
+models.py, views.py.  And in fact, only 'settings' is referenced here
+directly -- everything else is controlled from there.
+
+"""
+
+# Standard Python imports.
+import os
+import sys
+import logging
+from server import app
+# Google App Engine imports.
+from google.appengine.ext.webapp import util
+from flask import Flask
+
+def main():  
+  # Run the WSGI CGI handler with that application.
+  util.run_wsgi_app(app)
+
+if __name__ == '__main__':
+  main()
+
